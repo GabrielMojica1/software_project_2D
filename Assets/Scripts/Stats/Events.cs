25dfc4328fdeef21dc073de537c3adf68108ae0b
axle-m <alemar27@bergen.org>
2026-03-07 18:53:46 -0500

    Adds EventBus broker and example events, next is stats tracker

---
+++ Assets/Scripts/Stats/Events.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Events
+{
+    // events we want to track go in here
+    // I made some examples, but whichever events 
+    // we want to track can be added as needed
+    public struct EnemyKilledEvent
+    {
+        public string EnemyType;
+
+        public EnemyKilledEvent(string type)
+        {
+            EnemyType = type;
+        }
+    }
+
+    public struct DamageDealtEvent
+    {
+        public int Amount;
+
+        public DamageDealtEvent(int amount)
+        {
+            Amount = amount;
+        }
+    }
+
+    public struct ItemCollectedEvent
+    {
+        public string ItemID;
+
+        public ItemCollectedEvent(string id)
+        {
+            ItemID = id;
+        }
+    }
+
+    public struct LevelCompletedEvent
+    {
+        public int LevelNumber;
+
+        public LevelCompletedEvent(int level)
+        {
+            LevelNumber = level;
+        }
+    }
+}
