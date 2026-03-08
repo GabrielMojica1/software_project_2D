644777f07b6deef7592f5e584289f9bfb10e20e8
axle-m <alemar27@bergen.org>
2026-03-07 20:54:06 -0500

    Start button works, GameManager correctly switches scenes when needed

--- Assets/Scripts/GameStartButtonHandler.cs
+++ Assets/Scripts/GameStartButtonHandler.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameStartButtonHandler : MonoBehaviour
 {
-     public void OnButtonPressed()
+    public Button m_StartButton;
+
+     void Start()
+    {
+        m_StartButton.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
     {
         Debug.Log("Start button pressed, starting game...");
         // could probably be handled by EventBus but this is simple enough and will probably do for now
